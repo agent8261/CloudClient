832faac15905c1cad8367b24988ee6cf77aaa506
Darren Robinson <agent8261@yahoo.com>
2013-09-14 12:48:49 -0400

    Failed Connection

--- src/com/cloud/client/CloudClientActivity.java
+++ src/com/cloud/client/CloudClientActivity.java
@@ -1,12 +1,28 @@
 package com.cloud.client;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient.ConnectionCallbacks;
+import com.google.android.gms.common.GooglePlayServicesClient.OnConnectionFailedListener;
+import com.google.android.gms.plus.PlusClient;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.IntentSender.SendIntentException;
 import android.view.Menu;
 
-public class CloudClientActivity extends Activity
+public class CloudClientActivity extends Activity implements
+  ConnectionCallbacks, OnConnectionFailedListener
 {
+  private static final int REQUEST_CODE_RESOLVE_ERR = 9000;
 
+  private ProgressDialog mConnectionProgressDialog;
+  private PlusClient mPlusClient;
+  private ConnectionResult mConnectionResult;
+
+  // ---------------------------------------------------------------------------
+  // ---------------------------------------------------------------------------
+  
   @Override
   protected void onCreate(Bundle savedInstanceState)
   {
@@ -14,6 +30,7 @@ public class CloudClientActivity extends Activity
     setContentView(R.layout.cloud_client_activity);
   }
 
+  // ---------------------------------------------------------------------------
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu)
@@ -21,6 +38,47 @@ public class CloudClientActivity extends Activity
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.cloud_client, menu);
     return true;
+  }
+
+  // ---------------------------------------------------------------------------
+  
+  @Override
+  public void onConnectionFailed(ConnectionResult result)
+  {
+    if(mConnectionProgressDialog.isShowing() && result.hasResolution())
+    {
+      // The user clicked the sign-in button already. Start to resolve
+      // connection errors. Wait until onConnected() to dismiss the
+      // connection dialog.
+        try
+        {
+          result.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+        }
+        catch( SendIntentException e )
+        {
+          mPlusClient.connect();
+        }
+    }
+    // Save the intent so that we can start an activity when the user clicks
+    // the sign-in button.
+    mConnectionResult = result;
+  }
+
+  // ---------------------------------------------------------------------------
+  
+  @Override
+  public void onConnected(Bundle connectionHint) 
+  { // We've resolved any connection errors.
+    mConnectionProgressDialog.dismiss();
+  }
+
+
+  // ---------------------------------------------------------------------------
+  
+  @Override
+  public void onDisconnected()
+  {
+    
   }
 
 }
